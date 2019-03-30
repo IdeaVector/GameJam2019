957e1d5a91f38cebbb5df778deb156e5cb0ebaad
Neirwin <nightablackstone@gmail.com>
2019-03-30 21:41:45 +0300

    Added some super dumb and simple spawn function

---
+++ Assets/Scripts/SpawnGroundScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGroundScript : MonoBehaviour
+{
+
+    public GameObject[] groundObj;
+    public float spawnMinTime = 1f;
+    public float spawnMaxTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        Instantiate(groundObj[Random.Range(0, groundObj.GetLength(0))], transform.position, Quaternion.identity);
+        invoke("Spawn", Random.Range(spawnMinTime, spawnMaxTime));
+    }
+}
