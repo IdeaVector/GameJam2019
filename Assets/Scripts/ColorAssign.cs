95e06db2807ee2e05773b1151a7c776580023219
TimurSirkin <timur_sirkin@mail.ru>
2019-03-30 23:41:07 +0300

    Add picture collision logic

---
+++ Assets/Scripts/ColorAssign.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorAssign : MonoBehaviour
+{
+    public Color[] colors;
+    void Start()
+    {
+        Color currentColor = colors[Random.Range(0, colors.Length)];
+        GetComponent<PictureScript>().color = currentColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
