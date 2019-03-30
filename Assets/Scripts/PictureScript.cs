d440a03596fbe14caf96fce3c5e8d0476e34dc4e
TimurSirkin <timur_sirkin@mail.ru>
2019-03-30 21:47:05 +0300

    test

--- Assets/Scripts/PictureScript.cs
+++ Assets/Scripts/PictureScript.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PictureScript : MonoBehaviour
 {
+    public GameObject newSprite;
     private SpriteRenderer rend;
     public Color color;
 
     void Start()
     {
-        color = Color.red;
-        Sprite sprite = GetComponent<Sprite>();
-        //rend = sprite.GetComponent<SpriteRenderer>();
-        rend.color = color;
+        
+        ////color = Color.red;
+        //Sprite sprite = GetComponent<Sprite>();
+        //sprite = newSprite.GetComponent<SpriteRenderer>().sprite;
+        ////transform.GetComponentInChildren<SpriteRenderer>().color = Color.green;
     }
     
     void Update()
--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour
 {
-    private Vector2 speed = new Vector2(2,0);
+   // private Vector2 speed = new Vector2(2,0);
     private Rigidbody2D rb2d;
+    private Animator anim;
+    private bool isGround = false;
+    public Transform groundCheck;
+    private float groundRadius = 0.2f;
+    private float speed = 5f;
+    public LayerMask whatIsGround;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
-
+    private void Update()
+    {
+        if (isGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rb2d.AddForce(new Vector2(0, 600));
+        }
+    }
     private void FixedUpdate()
     {
-        rb2d.velocity = speed;
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb2d.AddForce(new Vector2(0, 90), ForceMode2D.Impulse);
-        }
+        isGround = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", isGround);
+        anim.SetFloat("VSpeed", rb2d.velocity.y);
+        if (!isGround)
+            return;
+        anim.SetFloat("Speed", Mathf.Abs(1));
+        rb2d.velocity = new Vector2( speed, rb2d.velocity.y);
     }
 }
