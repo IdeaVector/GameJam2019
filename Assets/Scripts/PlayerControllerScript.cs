980f2dc33d7de3185c56c71251bfc8893ae49621
Artemii Morozov <33790882+morozzArt@users.noreply.github.com>
2019-03-30 20:09:27 +0300

    add simple controll

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    private Vector2 speed = new Vector2(2,0);
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = speed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(new Vector2(0, 90), ForceMode2D.Impulse);
+        }
+    }
+}
