67f615f418c351a8f6bd15533403fad57803517f
TimurSirkin <timur_sirkin@mail.ru>
2019-03-30 19:19:07 +0300

    Implemate start game scenes

---
+++ Assets/Scripts/StartSceneScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartSceneScript : MonoBehaviour
+{
+    public Text firstLabel;
+    public Text secondLabel;
+    public Text thirdLabel;
+    public Text fouthLabel;
+    private float alpha = 0f;
+    private Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = firstLabel.color;
+        color.a = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        showLabels();
+    }
+    
+
+    void showLabels()
+    {
+        if (alpha < 100f)
+        {
+            alpha += 0.002f;
+            color.a = alpha;
+            firstLabel.color = color;
+            secondLabel.color = color;
+            thirdLabel.color = color;
+            fouthLabel.color = color;
+        }
+    }
+
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
