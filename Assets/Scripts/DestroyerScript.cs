9f8261ed938d6d76405f0e7ceaa4dcdb6b2007c7
Neirwin <nightablackstone@gmail.com>
2019-03-30 23:03:44 +0300

    Something's working

---
+++ Assets/Scripts/DestroyerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Break();
+        }
+        if (other.gameObject.transform.parent)
+        {
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
