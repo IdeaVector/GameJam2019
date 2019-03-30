224958acb57baec0ebb81022518d6d6ee6702254
TimurSirkin <timur_sirkin@mail.ru>
2019-03-31 02:51:35 +0300

    End with death mechanic

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -12,6 +12,9 @@ public class GameManagerScript : MonoBehaviour
     public Sprite middleFace;
     public Sprite defaultFace;
     public Sprite oldFace;
+
+    public Text text;
+
     private int ageIndex = 0;
     private int age = 20;
 
@@ -28,13 +31,52 @@ public class GameManagerScript : MonoBehaviour
 
     public void NextFace()
     {
-        faceImage.sprite = spriteList[ageIndex];
-        currentSprite.GetComponent<SpriteRenderer>().sprite = spriteList[ageIndex];
         ageIndex++;
-
-        if(ageIndex >3)
+        if (ageIndex > 3)
         {
             SceneManager.LoadScene("Menu");
+        }
+        if (ageIndex <= 3)
+        currentSprite.GetComponent<SpriteRenderer>().sprite = spriteList[ageIndex];
+        faceImage.sprite = spriteList[ageIndex];
+    }
+
+    public void AddAge(int value)
+    {
+        age += value;
+        if (ageIndex == 0 && age >= 35)
+        {
+            NextFace();
+        }
+        if (ageIndex == 1 && age >= 50)
+        {
+            NextFace();
+        }
+        if (ageIndex == 2 && age >= 65)
+        {
+            NextFace();
+        }
+        if (ageIndex == 3 && age >= 80)
+        {
+            NextFace();
+        }
+        text.text = age.ToString();
+    }
+
+    private bool isReady = true;
+
+    private int counter = 0;
+
+    private void Update()
+    {
+        if (counter == 0)
+        {
+            AddAge(3);
+        }
+        counter++;
+        if (counter >= 40)
+        {
+            counter = 0;
         }
     }
 }
