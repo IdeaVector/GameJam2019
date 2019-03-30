b7507a1ad452e0b0332d00a04268887f79c1875e
TimurSirkin <timur_sirkin@mail.ru>
2019-03-31 01:00:55 +0300

    Edit scene

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public Image faceImage;
+    public Sprite yangFace;
+    public Sprite middleFace;
+    public Sprite defaultFace;
+    public Sprite oldFace;
+    private int ageIndex = 0;
+
+    private List<Sprite> spriteList;
+
+    void Start()
+    {
+        spriteList = new List<Sprite>();
+        spriteList.Add(yangFace);
+        spriteList.Add(middleFace);
+        spriteList.Add(defaultFace);
+        spriteList.Add(oldFace);
+
+        NextFace();
+        NextFace();
+    }
+
+    public void NextFace()
+    {
+        faceImage.sprite = spriteList[ageIndex];
+        ageIndex++;
+    }
+}
