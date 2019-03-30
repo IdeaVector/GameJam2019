3b6ce972af1f200d75e7800d71159fd4421a406d
TimurSirkin <timur_sirkin@mail.ru>
2019-03-30 20:23:54 +0300

    Add BrushScript

---
+++ Assets/Scripts/BrushScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class BrushScript : MonoBehaviour
+{
+    public Color color = Color.white;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = color; 
+        Destroy(gameObject, 3);
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (!coll.isTrigger) // чтобы пуля не реагировала на триггер
+        {
+            switch (coll.tag)
+            {
+                case "Enemy_1":
+                    // что-то...
+                    break;
+                case "Enemy_2":
+                    // что-то еще...
+                    break;
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
