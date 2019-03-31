f714e8a584d0e6b95448ca36e82fd2c9560b1523
TimurSirkin <timur_sirkin@mail.ru>
2019-03-31 05:26:01 +0300

    Add background

--- Assets/Scripts/Background2D.cs
+++ Assets/Scripts/Background2D.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 
 public class Background2D : MonoBehaviour
 {
-    public Transform BG1;
-    public float speedBG1 = 0.005f;
-
-    public Transform BG2;
-    public float speedBG2 = 0.010f;
-
-    public Transform BG3;
-    public float speedBG3 = 0.015f;
+    public GameObject obj;
+    private Transform BG;
+    public float speedBG = 0.005f;
 
     private Camera cam;
 
     void Awake()
     {
         cam = GetComponent<Camera>();
+        BG = obj.transform;
     }
 
     void Start()
@@ -33,8 +29,6 @@ public class Background2D : MonoBehaviour
 
     void Update()
     {
-        if (BG1) BG1.position = GetVector(BG1.position, speedBG1);
-        if (BG2) BG2.position = GetVector(BG2.position, speedBG2);
-        if (BG3) BG3.position = GetVector(BG3.position, speedBG3);
+        if (BG) BG.position = GetVector(BG.position, speedBG);
     }
 }
