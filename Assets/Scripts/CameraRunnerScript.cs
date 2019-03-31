8d6769aa840e7795553ed729ac95ffdc4389ceac
Artemii Morozov <33790882+morozzArt@users.noreply.github.com>
2019-03-31 04:51:45 +0300

    Remake spawn

--- Assets/Scripts/CameraRunnerScript.cs
+++ Assets/Scripts/CameraRunnerScript.cs
@@ -7,8 +7,8 @@ public class CameraRunnerScript : MonoBehaviour
     public Transform player;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = new Vector3(player.position.x + 6, 0, -10); 
+            transform.position = new Vector3(player.position.x + 6, 0, -10); 
     }
 }
