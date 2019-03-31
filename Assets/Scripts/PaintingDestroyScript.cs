596da97a2c653f3851a52e0622244ea48d7876ff
TimurSirkin <timur_sirkin@mail.ru>
2019-03-31 04:31:27 +0300

    Add painting  destroying

---
+++ Assets/Scripts/PaintingDestroyScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintingDestroyScript : MonoBehaviour
+{
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            if (transform.position.x + 2 < player.transform.position.x)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
